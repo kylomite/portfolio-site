d6298f28d34f161926151e744fbedfe1fb323410
krylomite <kdelaney99@gmail.com>
2025-01-04 11:25:18 -0500

    add project details

--- src/assets/Data.ts
+++ src/assets/Data.ts
@@ -1,39 +1,76 @@
+import RancidLogo from "../assets/RancidTomatillos/RancidTomatillosLogo.jpeg";
+import RancidGif from "../assets/RancidTomatillos/RancidTomatillos.gif";
+import RancidData from "../assets/RancidTomatillos/RancidTomatillosDataDiagram.png";
+import TTradeLogo from "../assets/TTrade/TTradeLogo.png";
+import TTradeGif from "../assets/TTrade/t-trade_gif.gif";
+import TrackerLogo from "../assets/images/TrackerLogo.png";
+import PokeGotchiLogo from "../assets/images/PokeGotchiLogo.png";
+import GymCrushLogo from "../assets/GymCrush/GymCrushLogo.png";
+
 const Projects = [
     {
       Title: "GymCrush",
-      Images: "",
+      Logo: {GymCrushLogo},
+      Images: [],
       Description: "",
-
+      Tech: {
+        FE: [],
+        BE: [],
+        Tools: []
+      },
+      Team: "Solo",
+      Time: "On going"
     },
     {
       Title: "Job Tracker",
-      Images: "",
+      Logo: "",
+      Images: [],
       Description: "",
-
-    },
-    {
-      Title: "Tea Trade",
-      Images: "",
-      Description: "",
-
-    },
-    {
-      Title: "Rancid Tomatillos",
-      Images: "",
-      Description: "",
-
+      Tech: {
+        FE: ["Typescript", "React", "Tailwind", "Cypress"],
+        BE: ["Ruby on Rails", "RSpec"],
+        Tools: ["Git", "Github", "Slack"]
+      },
+      Team: "14 Members",
+      Time: "DEC 2024 - JAN 2025"
     },
     {
       Title: "Poke-Gotchi",
-      Image: "",
-      Description: "",
-
+      Logo: "",
+      Image: [],
+      Description: "Poké-Gochi, an interactive app where Trainers can interact, level up, and train virtual Pokémon. The main goal with this project was to build something from the ground up collaboratively with my team members. I had the opportunity to explore stretch tech by implementation of asynchronous jobs through Redis and Sidekiq to periodically update database values. My primary responsibilities on this project were centered on BE functionality, stretch tech implementation, and deployment",
+      Tech: {
+        FE: [],
+        BE: [ "Ruby", "Ruby on Rails", "sidekiq"],
+        Tools: ["Heroku", "Redis"]
+      },
+      Team: "5 Members",
+      Time: "OCT 2024 - NOV 2024"
     },
     {
-      Title: "Futbol Stat Tracker",
-      Image: "",
+      Title: "T-Trade",
+      Logo: {TTradeLogo},
+      Images: [{TTradeGif}],
+      Description: "T-Trade is an E-commerce website designed for vendors to view, search and edit their subscribers' subscriptions to tea deliveries. This project had a short 72 timeline. I prioritized time management as I worked on this full stack project while maintaining a focus on accessibility and responsiveness in my design. The application uses the Chrome Dev Tools Lighthouse feature to test accessibility achieving a 100% score.",
+      Tech: {
+        FE: ["HTML", "CSS", "Javascript", "React"],
+        BE: ["Ruby", "Ruby On Rails"],
+        Tools: ["Git", "Github"]
+      },
+      Team: "Solo",
+      Time: "NOV 2024"
+    },
+    {
+      Title: "Rancid Tomatillos",
+      Logo: "",
+      Images: [],
       Description: "",
-
+      Tech: {
+        FE: [],
+        Tools: []
+      },
+      Team: "Pair",
+      Time: "OCT 2024"
     },
 ]
 export const Skills = [
--- src/components/ProjectContainer.tsx
+++ src/components/ProjectContainer.tsx
@@ -1,159 +1,165 @@
-import React, { useRef, useEffect } from "react";
-import RancidLogo from "../assets/images/RancidTomatillosLogo.jpeg";
-import TeaTradeLogo from "../assets/images/teaTrade.png";
-import TrackerLogo from "../assets/images/TrackerLogo.png";
-import FutbolStatTrackerLogo from "../assets/images/FutbolStatTrackerLogo.png";
-import PokeGotchiLogo from "../assets/images/PokeGotchiLogo.png";
-import GymCrushLogo from "../assets/images/GymCrushLogo.png"
+// import React, { useRef, useEffect } from "react";
+// import RancidLogo from "../assets/images/RancidTomatillosLogo.jpeg";
+// import TeaTradeLogo from "../assets/images/teaTrade.png";
+// import TrackerLogo from "../assets/images/TrackerLogo.png";
+// import FutbolStatTrackerLogo from "../assets/images/FutbolStatTrackerLogo.png";
+// import PokeGotchiLogo from "../assets/images/PokeGotchiLogo.png";
+// import GymCrushLogo from "../assets/images/GymCrushLogo.png"
 
-const ProjectContainer: React.FC = () => {
-  const trackRef = useRef<HTMLDivElement | null>(null);
+// const ProjectContainer: React.FC = () => {
+//   const trackRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const track = trackRef.current;
-    if (!track) return;
+//   useEffect(() => {
+//     const track = trackRef.current;
+//     if (!track) return;
 
-    let mouseDownAt = 0;
-    let prevPercentage = 0;
-    let isMouseDown = false;
+//     let mouseDownAt = 0;
+//     let prevPercentage = 0;
+//     let isMouseDown = false;
 
-    const handleOnDown = (e: MouseEvent) => {
-      isMouseDown = true;
-      mouseDownAt = e.clientX;
-    };
+//     const handleOnDown = (e: MouseEvent) => {
+//       isMouseDown = true;
+//       mouseDownAt = e.clientX;
+//     };
 
-    const handleOnUp = () => {
-      isMouseDown = false;
-      prevPercentage = parseFloat(track.dataset.percentage || "0");
-    };
+//     const handleOnUp = () => {
+//       isMouseDown = false;
+//       prevPercentage = parseFloat(track.dataset.percentage || "0");
+//     };
 
-    const handleOnMove = (e: MouseEvent) => {
-      if (!isMouseDown) return;
+//     const handleOnMove = (e: MouseEvent) => {
+//       if (!isMouseDown) return;
 
-      const mouseDelta = e.clientX - mouseDownAt;
-      const maxDelta = window.innerWidth / 2;
-      const percentage = (mouseDelta / maxDelta) * 100;
-      const nextPercentageUnconstrained = prevPercentage + percentage;
+//       const mouseDelta = e.clientX - mouseDownAt;
+//       const maxDelta = window.innerWidth / 2;
+//       const percentage = (mouseDelta / maxDelta) * 100;
+//       const nextPercentageUnconstrained = prevPercentage + percentage;
 
-      let nextPercentage = Math.max(nextPercentageUnconstrained, -(track.children.length - 1) * 5.75);
-      nextPercentage = Math.min(nextPercentage, 0);
+//       let nextPercentage = Math.max(nextPercentageUnconstrained, -(track.children.length - 1) * 5.75);
+//       nextPercentage = Math.min(nextPercentage, 0);
 
-      track.dataset.percentage = nextPercentage.toString();
+//       track.dataset.percentage = nextPercentage.toString();
 
-      track.animate(
-        {
-          transform: `translate(${nextPercentage}%, -50%)`,
-        },
-        { duration: 1200, fill: "forwards" }
-      );
+//       track.animate(
+//         {
+//           transform: `translate(${nextPercentage}%, -50%)`,
+//         },
+//         { duration: 1200, fill: "forwards" }
+//       );
 
-      for (const image of track.getElementsByClassName("image")) {
-        (image as HTMLImageElement).animate(
-          {
-            objectPosition: `${100 + nextPercentage}% center`,
-          },
-          { duration: 1200, fill: "forwards" }
-        );
-      }
-    };
+//       for (const image of track.getElementsByClassName("image")) {
+//         (image as HTMLImageElement).animate(
+//           {
+//             objectPosition: `${100 + nextPercentage}% center`,
+//           },
+//           { duration: 1200, fill: "forwards" }
+//         );
+//       }
+//     };
 
-    const triggerInitialAnimation = () => {
-      const initialPercentage = 0;
-      track.dataset.percentage = initialPercentage.toString();
-      track.style.transform = `translate(${initialPercentage}%, 0%%)`;
+//     const triggerInitialAnimation = () => {
+//       const initialPercentage = 0;
+//       track.dataset.percentage = initialPercentage.toString();
+//       track.style.transform = `translate(${initialPercentage}%, 0%%)`;
 
-      for (const image of track.getElementsByClassName("image")) {
-        (image as HTMLImageElement).style.objectPosition = `${100 + initialPercentage}% center`;
-      }
-    };
+//       for (const image of track.getElementsByClassName("image")) {
+//         (image as HTMLImageElement).style.objectPosition = `${100 + initialPercentage}% center`;
+//       }
+//     };
 
-    triggerInitialAnimation();
+//     triggerInitialAnimation();
 
-    window.addEventListener("mousedown", handleOnDown);
-    window.addEventListener("mouseup", handleOnUp);
-    window.addEventListener("mousemove", handleOnMove);
+//     window.addEventListener("mousedown", handleOnDown);
+//     window.addEventListener("mouseup", handleOnUp);
+//     window.addEventListener("mousemove", handleOnMove);
 
-    return () => {
-      window.removeEventListener("mousedown", handleOnDown);
-      window.removeEventListener("mouseup", handleOnUp);
-      window.removeEventListener("mousemove", handleOnMove);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener("mousedown", handleOnDown);
+//       window.removeEventListener("mouseup", handleOnUp);
+//       window.removeEventListener("mousemove", handleOnMove);
+//     };
+//   }, []);
 
-  return (
+//   return (
+//     <section className=" mt-80 w-screen overflow-hidden flex flex-col items-center justify-center bg-[rgba(209,213,219,0.4)]">
+//       <h1 className=" mt-4 mb-2 text-2xl font-semibold">Projects</h1>
+
+//       <div
+//         className="relative w-full h-[68vmin] overflow-hidden"
+//         style={{ width: "100%", height: "68vmin" }}
+//       >
+//         <div
+//           id="image-track"
+//           ref={trackRef}
+//           data-mouse-down-at="0"
+//           data-prev-percentage="0"
+//           className="flex gap-[4vmin] absolute left-0 top-1/2"
+//           style={{
+//             transform: "translate(0%, -50%)",
+//             userSelect: "none",
+//           }}
+//         >
+//           <div className="ml-8 relative w-[40vmin] h-[56vmin]">
+//             <img
+//               className="image w-full h-full object-cover object-center"
+//               src={GymCrushLogo}
+//               draggable="false"
+//             />
+//             <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Gym Crush</h2>
+//           </div>
+//           <div 
+//           className="relative w-[40vmin] h-[56vmin]"
+//           >
+//             <img
+//               className="image w-full h-full object-cover object-center"
+//               // src="tets"
+//               src={TrackerLogo}
+//               draggable="false"
+//             />
+//             <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Job Tracker</h2>
+//           </div>
+//           <div className="relative w-[40vmin] h-[56vmin]">
+//             <img
+//               className="image w-full h-full object-cover object-center"
+//               src={TeaTradeLogo}
+//               draggable="false"
+//             />
+//             <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> T-Trade</h2>
+//           </div>
+//           <div className="relative w-[40vmin] h-[56vmin] ">
+//             <img
+//               className="image w-full h-full object-cover object-center"
+//               src={RancidLogo}
+//               draggable="false"
+//             />
+//             <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Rancid Tomatillos</h2>
+//           </div>
+//           <div className="relative w-[40vmin] h-[56vmin] ">
+//             <img
+//               className="image w-full h-full object-cover object-center"
+//               src={PokeGotchiLogo}
+//               draggable="false"
+//             />
+//             <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Poke-Gotchi</h2>
+//           </div>
+//           <div className="relative w-[40vmin] h-[56vmin] ">
+//             <img
+//               className="image w-full h-full object-cover object-center"
+//               src={FutbolStatTrackerLogo}
+//               draggable="false"
+//             />
+//             <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Futbol Stat Tracker</h2>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+function ProjectContainer() {
+  return(
     <section className=" mt-80 w-screen overflow-hidden flex flex-col items-center justify-center bg-[rgba(209,213,219,0.4)]">
-      <h1 className=" mt-4 mb-2 text-2xl font-semibold">Projects</h1>
-
-      <div
-        className="relative w-full h-[68vmin] overflow-hidden"
-        style={{ width: "100%", height: "68vmin" }}
-      >
-        <div
-          id="image-track"
-          ref={trackRef}
-          data-mouse-down-at="0"
-          data-prev-percentage="0"
-          className="flex gap-[4vmin] absolute left-0 top-1/2"
-          style={{
-            transform: "translate(0%, -50%)",
-            userSelect: "none",
-          }}
-        >
-          <div className="ml-8 relative w-[40vmin] h-[56vmin]">
-            <img
-              className="image w-full h-full object-cover object-center"
-              src={GymCrushLogo}
-              draggable="false"
-            />
-            <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Gym Crush</h2>
-          </div>
-          <div 
-          className="relative w-[40vmin] h-[56vmin]"
-          >
-            <img
-              className="image w-full h-full object-cover object-center"
-              // src="tets"
-              src={TrackerLogo}
-              draggable="false"
-            />
-            <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Job Tracker</h2>
-          </div>
-          <div className="relative w-[40vmin] h-[56vmin]">
-            <img
-              className="image w-full h-full object-cover object-center"
-              src={TeaTradeLogo}
-              draggable="false"
-            />
-            <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> T-Trade</h2>
-          </div>
-          <div className="relative w-[40vmin] h-[56vmin] ">
-            <img
-              className="image w-full h-full object-cover object-center"
-              src={RancidLogo}
-              draggable="false"
-            />
-            <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Rancid Tomatillos</h2>
-          </div>
-          <div className="relative w-[40vmin] h-[56vmin] ">
-            <img
-              className="image w-full h-full object-cover object-center"
-              src={PokeGotchiLogo}
-              draggable="false"
-            />
-            <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Poke-Gotchi</h2>
-          </div>
-          <div className="relative w-[40vmin] h-[56vmin] ">
-            <img
-              className="image w-full h-full object-cover object-center"
-              src={FutbolStatTrackerLogo}
-              draggable="false"
-            />
-            <h2 className="flex justify-center border-t-4 border-slate-900 pt-1 text-xl"> Futbol Stat Tracker</h2>
-          </div>
-        </div>
-      </div>
+           <h1 className=" mt-4 mb-2 text-2xl font-semibold">Projects</h1>
     </section>
-  );
-};
-
+  )
+}
 export default ProjectContainer;
