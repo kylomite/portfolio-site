0fa466a7a6c342a5321ed0dd2e08436ee392f277
kylomite <kdelaney99@gmail.com>
2025-03-31 15:18:01 -0400

    update resume styling

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <div className="pt-32 sm:pt-20 md:pt-16 w-screen flex flex-col items-center">
       <div className="text-center p-6 w-full">
-        <h1 className="text-4xl font-bold mb-4">Resume</h1>
+        <h1 className="text-4xl font-bold mb-4 mt-6">Resume</h1>
         <div className="w-full flex justify-center">
           <object
             data={Res}
